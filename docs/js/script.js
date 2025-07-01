ca63161409607aeb84bf2d283ce77aa56c937dff
Douglas Sheysson <douglas.sheysson@gmai.com~>
2025-07-01 01:03:24 -0300

    ajustes

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,17 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     // Mapeamento dos IDs dos botões para os NOMES DE ARQUIVOS HTML (ou URLs completas)
     const pageMap = {
         'btnQuemCriouPython': 'HORADECODAR/docs/quem-criou-python.html', 
-        'btnSobrePython': '/sobre-python.html',         
-        'btnRecursos': '/recursos.html',               
-        'btnTutoriais': '/tutoriais.html',             
-        'btnComunidade': '/comunidade.html',           
-        'btnProjetos': '/projetos.html',              
-        'btnFerramentas': '/ferramentas.html',         
-        'btnNoticias': '/noticias.html',               
-        'btnEventos': '/eventos.html',                 
-        'btnContato': '/contato.html',                 
-        'btnFAQ': '/faq.html',                         
-        'btnTermos': '/termos.html'                    
+        'btnSobrePython':     'HORADECODAR/docs/sobre-python.html',         
+        'btnRecursos':        'HORADECODAR/docs/recursos.html',               
+        'btnTutoriais':       'HORADECODAR/docs/tutoriais.html',             
+        'btnComunidade':      'HORADECODAR/docs/comunidade.html',           
+        'btnProjetos':        'HORADECODAR/docs/projetos.html',              
+        'btnFerramentas':     'HORADECODAR/docs/ferramentas.html',         
+        'btnNoticias':        'HORADECODAR/docs/noticias.html',               
+        'btnEventos':         'HORADECODAR/docs/eventos.html',                 
+        'btnContato':         'HORADECODAR/docs/contato.html',                 
+        'btnFAQ':             'HORADECODAR/docs/faq.html',                         
+        'btnTermos':          'HORADECODAR/docs/termos.html'                    
     };
  
     // Percorre cada entrada no mapa para adicionar o event listener
